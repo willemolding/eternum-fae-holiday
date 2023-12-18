76bdfc1e364b83ade1a2714720b9b91df15c80ac
Willem Olding <willemolding@gmail.com>
2023-12-18 17:49:28 +1100

    add skeleton of shuttle server

--- ministry-of-fey-secrets/src/main.rs
+++ ministry-of-fey-secrets/src/main.rs
@@ -1,16 +1,32 @@
-use axum::{routing::get, Router};
-use shuttle_secrets::SecretStore;
+use std::sync::Arc;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+use axum::{extract::Path, routing::get, Router};
+use shuttle_secrets::SecretStore;
+use starknet::providers::{jsonrpc::HttpTransport, JsonRpcClient};
+
+async fn process_gifts(
+    Path((block_hash, caller_address)): Path<(String, String)>,
+    provider: Arc<JsonRpcClient<HttpTransport>>,
+) -> &'static str {
+    // grab the events from the block
+
+    // filter by the caller and event type
+
+    "Yep"
 }
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
+    let rpc_url = url::Url::parse(&secret_store.get("STARKNET_RPC_URL").unwrap()).unwrap();
+    let provider = Arc::new(JsonRpcClient::new(HttpTransport::new(rpc_url)));
 
-    let url = secret_store.get("KATANA_URL").unwrap();
-
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route(
+        "/:block_hash/:caller_address",
+        get({
+            let provider = Arc::clone(&provider);
+            move |path| process_gifts(path, provider)
+        }),
+    );
 
     Ok(router.into())
 }
